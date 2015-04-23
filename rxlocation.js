5529b9d0e64f1e3679e5e463440ca12ea7d75206
Andrew Hao <andrewhao@gmail.com>
2015-04-23 10:02:18 -0700

    Adding a couple of higher order functions to the stream.

--- rxlocation.js
+++ rxlocation.js
@@ -17,9 +17,35 @@ gpxSource.on('readable', function() {
 
 var stream = Bacon.sequentially(2000, points);
 
+var historicalStream = stream.slidingWindow(2)
+
 stream.onValue(function(loc) {
 	console.log(loc);
 });
 
+velocityStream = historicalStream.map(function(history) {
+  if(history.length < 2) { return }
+  var p1 = history[0]
+  var p2 = history[1]
 
+  var dist = gpsUtil.getDistance(p1.lon, p1.lat, p2.lon, p2.lat)
+  console.log("distance between points is:" + dist);
+
+  var t1 = Date.parse(p1.time);
+  var t2 = Date.parse(p2.time);
+
+  var delta = t2 - t1;
+  var seconds = delta / 1000;
+
+  console.log("time elapsed is: %ss", seconds);
+
+  var velocity = dist / seconds;
+
+
+  return velocity;
+});
+
+velocityStream.subscribe(function(velocity) {
+  console.log("instantaneous velocity is: %s m/s", velocity)
+});
 
